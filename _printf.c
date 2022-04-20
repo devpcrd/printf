6c3bcc5065e665eb49ff3ec30c81ed1704a5e775
devpcrd <infinitepeco@gmail.com>
2022-04-20 06:25:39 +0530

    fix _printf.c

--- _printf.c
+++ _printf.c
@@ -1,17 +1,10 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _printf - takes an argument and prints to the standard output
- * considering only the %,c and s specifiers
- *
- * @format: pointer to the source string.
- *
- * Return: len
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
  */
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
 	convert_match m[] = {
 		{"%s", printf_string}, {"%c", printf_char},
@@ -20,33 +13,33 @@ int _printf(const char *format, ...)
 		{"%R", printf_rot13}, {"%b", printf_bin}, {"%u", printf_unsigned},
 		{"%o", printf_oct}, {"%x", printf_hex}, {"%X", printf_HEX},
 		{"%S", printf_exclusive_string}, {"%p", printf_pointer}
-	 };
+	};
 
-	va_list ls;
-	int x = 0, y = 0, len = 0;
+	va_list args;
+	int i = 0, j, len = 0;
 
-	va_start(ls, format);
+	va_start(args, format);
 	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
 		return (-1);
 
 Here:
-	while (format[x] != '\0')
+	while (format[i] != '\0')
 	{
-		y = 13;
-		while (y >= 0)
+		j = 13;
+		while (j >= 0)
 		{
-			if (m[y].id[0] == format[x] && m[y].id[1] == format[x + 1])
+			if (m[j].id[0] == format[i] && m[j].id[1] == format[i + 1])
 			{
-				len += m[y].f(ls);
-				x = x + 2;
+				len += m[j].f(args);
+				i = i + 2;
 				goto Here;
 			}
-			y--;
+			j--;
 		}
-		_putchar(format[x]);
+		_putchar(format[i]);
 		len++;
-		x++;
+		i++;
 	}
-	va_end(ls);
+	va_end(args);
 	return (len);
 }
